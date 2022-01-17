30fb4b91423cfec142ddc06cdd4b602256897402
Denis Sivolap <di@MacBook-Pro-2.local>
2022-01-17 23:39:05 +0400

    fix: Lesson 1, float types

--- lessons/lesson_1/triangle_area.rb
+++ lessons/lesson_1/triangle_area.rb
@@ -2,6 +2,6 @@ puts 'What is base of the triangle?'
 base = gets.chomp
 puts 'What is height of the triangle?'
 height = gets.chomp
-triangle_area = 0.5 * base.to_i * height.to_i
+triangle_area = 0.5 * base.to_f * height.to_f
 
 puts "Triangle area is #{triangle_area}"
--- lessons/lesson_1/triangle_type.rb
+++ lessons/lesson_1/triangle_type.rb
@@ -4,7 +4,7 @@ puts 'What is the second side of the triangle?'
 second_side = gets.chomp
 puts 'What is the third side of the triangle?'
 third_side = gets.chomp
-all_sides = [first_side.to_i, second_side.to_i, third_side.to_i].sort # To find hypotenuse and legs
+all_sides = [first_side.to_f, second_side.to_f, third_side.to_f].sort # To find hypotenuse and legs
 
 if all_sides.uniq.length == 2
   puts 'Your triangle is isosceles'
@@ -12,4 +12,6 @@ elsif all_sides.uniq.length == 1
   puts 'Your triangle is equilateral and isosceles'
 elsif all_sides[0]**2 + all_sides[1]**2 == all_sides[2]**2
   puts 'Your triangle is rectangular'
+else
+  puts 'Your triangle is NOT rectangular, equilateral or isosceles'
 end
